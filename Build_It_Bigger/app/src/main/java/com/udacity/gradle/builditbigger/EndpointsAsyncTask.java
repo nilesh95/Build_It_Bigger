04629c91c729b65754a46793d4b0b5e2b7e65038
nilesh95 <nayalneel@gmail.com>
2016-08-15 17:34:20 +0530

    endpoint async
    class created to make async call to app engine

--- Build_It_Bigger/app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ Build_It_Bigger/app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -30,7 +30,7 @@ public class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, S
                     // options for running against local devappserver
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - turn off compression when running against local devappserver
-                    .setRootUrl("http://192.168.1.5:8080/_ah/api/")
+                    .setRootUrl("https://buildit-140411.appspot.com/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
